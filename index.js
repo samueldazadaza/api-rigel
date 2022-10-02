c6c8388cd19f7b398653c9a55e991b70757b345f
sam <samueldazadaza@gmail.com>
2022-10-01 19:56:38 -0500

    dolar baco central chile

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from "express";
-import axios from "axios"
+import axios from "axios";
+import * as cheerio from "cheerio";
 const app = express();
 
 
 app.get("/", async (req, res) => {
     console.log("entro al try😎")
     try {
-        //const {data} = await axios.get("https://www.bcentral.cl/inicio")
-        const {data} = await axios.get("http://rigel.greenmovil.com.co:8080/RigelpbWS/disponibilidadFlota/todos?key=JM8xH8zNHX7XGsUe8rkDf")
+        const {data} = await axios.get("https://www.bcentral.cl/inicio")
+        //const {data} = await axios.get("http://rigel.greenmovil.com.co:8080/RigelpbWS/disponibilidadFlota/todos?key=JM8xH8zNHX7XGsUe8rkDf")
 
-        res.json({ data });
-        
+        const $ = cheerio.load(data)
+        const selectorDolar = "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(1) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2"
+
+        res.json({ dolar: $(selectorDolar).text() });
     } catch (error) {
         res.json({error})
     }

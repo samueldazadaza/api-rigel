418e4b6be57aa5667107cfab33a9a6109ad32e18
Samuel Daza <65508457+samueldazadaza@users.noreply.github.com>
2022-10-07 11:54:57 -0500

    Cambiar system name

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.get("/", cors(), async (req, res) => {
     console.log("entro al try😎")
     try {
         //const {data} = await axios.get("https://www.bcentral.cl/inicio")
-        const {data} = await axios.get("http://rigel.greenmovil.com.co:8080/RigelpbWS/disponibilidadFlota/todos?key=JM8xH8zNHX7XGsUe8rkDf")
-
+        const {dataits} = await axios.get("http://rigel.greenmovil.com.co:8080/RigelpbWS/disponibilidadFlota/todos?key=JM8xH8zNHX7XGsUe8rkDf")
+        const {dataits2} = await dataits.filter dataits.filter(system_name => system_name == "IT-ITS"); // return implicito
         //const $ = cheerio.load(data)
         //const selectorDolar = "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(1) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2"
 
         //res.json({ dolar: $(selectorDolar).text() });
-        res.json({ data });
+        res.json({ data, data2 });
     } catch (error) {
         res.json({error})
     }
